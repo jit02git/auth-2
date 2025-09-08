f3307ec976503cc23146d9c9dd838443b937b24b
jitesh <jitesh02sep@gmail.com>
2025-09-08 21:51:04 +0530

    promise

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,7 +4,14 @@ const addUser = (req, res) => {
     const { name, email, phone, address, city, state, country } = req.body;
 
     const user = new User(req.body);
-    console.log(user);
+
+    user.save().then(() => {
+        console.log('User added successfully');
+        res.status(201).json({ message: 'User added successfully', data: user });
+    }).catch((error) => {
+        console.error('Failed to add user:', error);
+        res.status(500).json({ error: 'Failed to add user' });
+    });
 }
 
 module.exports = {addUser}
