c628f9aee8da7e60091518c9be51cf7fb80c35b2
jitesh <jitesh02sep@gmail.com>
2025-09-09 21:54:28 +0530

    working on authentication

---
+++ model/Auth.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');    
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    confirmPassword: {
+        type: String,
+        required: true
+    },
+});
+
+const auth = mongoose.model('Auth', userSchema);
+
+module.exports = auth
+
+
--- route/UserRoute.js
+++ route/UserRoute.js
@@ -7,5 +7,6 @@ const app = express.Router();
 app.post('/add', UserController.addUser);
 app.post('/update/:id', UserController.userUpdate);
 app.get('/view/:id', UserController.viewUser);
+app.delete('/delete/:id', UserController.deleteUser);
 
 module.exports = app;
