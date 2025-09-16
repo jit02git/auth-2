114f3b98bb5391b62cc3cc8baf95a12907781edf
jitesh <jitesh02sep@gmail.com>
2025-09-16 21:12:12 +0530

    working on schemas

--- model/Auth.js
+++ model/Auth.js
@@ -20,8 +20,5 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
-userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
-}
 
 module.exports = mongoose.model('User', userSchema);
