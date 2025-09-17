e69113c089c65d54aadce6e6e0ad73757557db74
jitesh <jitesh02sep@gmail.com>
2025-09-18 00:09:13 +0530

    working on auth register

---
+++ controller/AuthController.js
@@ -0,0 +1,28 @@
+const User = require("../model/Auth");
+
+const register = async (req, res) => {
+  try {
+    const { name, email, password, phone, address, city, state, country } =
+      req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      city,
+      state,
+      country,
+    });
+
+    await user.save();
+    res
+      .status(201)
+      .json({ message: "User registered successfully", data: user });
+  } catch (error) {
+    console.error("Registration error:", error);
+    res.status(500).json({ error: "Server error" });
+  }
+};
--- model/Auth.js
+++ model/Auth.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');    
+const jwt = require('jsonwebtoken');   
+ 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -14,11 +15,5 @@ const userSchema = new Schema({
   country: { type: String, required: true },
 });
 
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
-
-
-module.exports = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
